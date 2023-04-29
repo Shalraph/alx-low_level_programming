c839c717e83345f421a9ee993ad7de7638510b6a
Shalraph <shalraph719@gmail.com>
2023-04-28 21:45:58 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,17 +2,13 @@
 #include "main.h"
 
 /**
-
 * main - Test function for positive or negative
-
 * Return: 0
-
 */
 
 int main(void)
 
 {
-
 int i;
 
 i = 0;
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,8 +1,6 @@
 
 #include <stdio.h>
-
 #include "main.h"
-
 /**
  * print_remaining_days - takes a date and prints how many
  *days are
@@ -38,17 +36,13 @@ printf("Remaining days: %d\n", 366 - day);
 else
 
 {
-
 if (month == 2 && day == 60)
-
 {
 
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 
 }
-
 else
-
 {
 
 printf("Day of the year: %d\n", day);
